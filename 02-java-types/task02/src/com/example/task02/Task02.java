0bbc550f402283c2ce4af378380cd9da1aec2f48
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2020-09-20 20:53:40 +0500

    Update Task02.java

--- 02-java-types/task02/src/com/example/task02/Task02.java
+++ 02-java-types/task02/src/com/example/task02/Task02.java
@@ -3,22 +3,10 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        try {
-            Byte.parseByte(input);
-        } catch (NumberFormatException e){
-            try {
-                Short.parseShort(input);
-            } catch (NumberFormatException g){
-                try {
-                    Integer.parseInt(input);
-                } catch (NumberFormatException d){
-                    return "long";
-                }
-                return "int";
-            }
-            return "short";
-        }
-        return "byte";
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return "";
     }
 
     public static void main(String[] args) {
